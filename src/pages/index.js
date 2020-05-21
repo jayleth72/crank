7e9a9ab942010676817e42f08203baf7610235d4
Jay Letheby <jay.letheby@gmail.com>
2020-05-21 22:02:51 +1000

    added background image optimisations for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Simplehero from "../components/SimpleHero"
+import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner"
 import { Link } from "gatsby"
 import About from "../components/home/About"
 import Services from "../components/home/Services"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({data}) => (
   <Layout>
-    <Simplehero>
+    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
       <Banner
         title="Crank with Hank Fitness"
         info="Bringing as many people as possible to Fitness!"
@@ -17,8 +18,20 @@ export default () => (
           Learn how to crank
         </Link>
       </Banner>
-    </Simplehero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
