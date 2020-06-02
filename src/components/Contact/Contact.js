08b3e5be9b645c14542475e6bf59c54589d2696f
Jay Letheby <jay.letheby@gmail.com>
2020-06-02 21:46:13 +1000

    added connection to contentful

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/contact.module.css"
+import { Formik } from "formik"
+import * as EmailValidator from "email-validator"
+import * as Yup from "yup"
 
 const Contact = () => {
   return (
@@ -10,46 +13,94 @@ const Contact = () => {
       <div className={styles.center}>
         <div>
           <p>
-            Feel free to get in touch with me!&nbsp;&nbsp;
-            I'll do my best to get back to you ASAP.
+            Feel free to get in touch with me!&nbsp;&nbsp; I'll do my best to
+            get back to you ASAP.
           </p>
         </div>
 
-        <form className={styles.form}>
-          <div>
-            <input action="https://formspree.io/jay.letheby@gmail.com"
-              method="POST"
-              type="text"
-              name="name"
-              id="name"
-              className={styles.formControl}
-              placeholder="your name"
-            />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className={styles.formControl}
-              placeholder="email@email.com"
-            />
-            <textarea
-              name="message"
-              id="message"
-              rows="10"
-              className={styles.formControl}
-              placeholder="your message"
-            />
-          </div>
-          <div>
-            <div>
-              <input
-                type="submit"
-                value="submit here"
-                className={styles.submit}
-              />
-            </div>
-          </div>
-        </form>
+        <Formik
+          initialValues={{ name: "", email: "", message: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log("Submitting")
+          }}
+          validationSchema={Yup.object.shape({
+            name: Yup.string().required("Please enter your name"),
+            email: Yup.string().email().required("Email is required"),
+            message: Yup.string().required("Please enter a message"),
+          })}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            } = props
+            return (
+              <form
+                action="https://formspree.io/jay.letheby@gmail.com"
+                method="POST"
+                onSubmit={handleSubmit}
+                className={styles.form}
+              >
+                <div>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={styles.formControl}
+                    placeholder="your name"
+                  />
+                  {errors.name && touched.name && (
+                    <div className="input-Feedback">errors.name</div>
+                  )}
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={styles.formControl}
+                    placeholder="email@email.com"
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-Feedback">errors.email</div>
+                  )}
+                  <textarea
+                    name="message"
+                    id="message"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.message}
+                    rows="10"
+                    className={styles.formControl}
+                    placeholder="your message"
+                  />
+                  {errors.message && touched.message && (
+                    <div className="input-Feedback">errors.message</div>
+                  )}
+                </div>
+                <div>
+                  <div>
+                    <input
+                      type="submit"
+                      value="submit here"
+                      disabled={isSubmitting}
+                      className={styles.submit}
+                    />
+                  </div>
+                </div>
+              </form>
+            )
+          }}
+        </Formik>
       </div>
     </section>
   )
