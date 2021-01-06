777f75e2df7cef1fbf8dd629dc0afe3c5a4c9e29
Jay Letheby <jay.letheby@gmail.com>
2021-01-06 14:29:52 +1030

    changed navbar logo to crank acronym

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import styles from "../css/navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
-import logo from "../images/crank_logo_white.png"
+import logo from "../images/crank_white_acro.png"
 
 const Navbar = () => {
   const [isOpen, setNav] = useState(false)
